ad5cd2dcb3adf717ea7de5f668d4a1af25b18e3e
MarcusV <Marcus-v@outlook.com>
2023-03-11 11:39:12 -0300

    create navigation logic

--- lib/App.dart
+++ lib/App.dart
@@ -10,8 +10,20 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+
+  int _current_index = 0;
+
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> windows = [
+      Text("1"),
+      Text("2"),
+      Text("3"),
+      Text("4"),
+    ];
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("StreamMV"),
@@ -24,7 +36,28 @@ class _AppState extends State<App> {
       ),
 
 
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [windows[_current_index]],
+        ),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: true,
+        currentIndex: _current_index,
+        onTap: (index){
+          setState(() {
+            _current_index = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.deepPurpleAccent,
+        items: [
+        BottomNavigationBarItem(label: "Home",icon: Icon(Icons.home), backgroundColor: Colors.grey[350]),
+        BottomNavigationBarItem(label: "Hot", icon: Icon(Icons.whatshot),backgroundColor:Colors.grey[350]),
+        BottomNavigationBarItem(label: "Subscriptions", icon: Icon(Icons.subscriptions),backgroundColor:Colors.grey[350]),
+        BottomNavigationBarItem(label: "Folder", icon: Icon(Icons.folder),backgroundColor:Colors.grey[350]),
+      ],),
 
     );
   }
